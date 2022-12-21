e9e37f61c0a39358512aada0a2947e01d4f5654d
vlrchy <aliracahya@gmail.com>
2022-12-21 23:39:57 +0700

    GoB

--- icon/js/plugins/Remove weapon armor key.js
+++
@@ -1,7 +0,0 @@
-Window_ItemCategory.prototype.maxCols = function() {
-    return 4;
-};
-Window_ItemCategory.prototype.makeCommandList = function() {
-    this.addCommand(TextManager.item,    'item');
-
-};
